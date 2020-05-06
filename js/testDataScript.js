c16224f870e7c524b20bec74bf7891a38e4a41c3
Yevheniia Drozdova <evgesha.drozdova@gmail.com>
2020-05-06 16:30:30 +0300

    додала зелений лаєр для 2019 року

--- js/testDataScript.js
+++ js/testDataScript.js
@@ -25,69 +25,47 @@ function getJSON(url, successHandler, errorHandler) {
     xhr.send();
 }
 
-
-
-
 var map = L.map('map').setView([49.49229399862877, 29.94335937500001], 7);
 
-L.tileLayer('https://tiles.stadiamaps.com/tiles/alidade_smooth/{z}/{x}/{y}{r}.png', {
-    subdomains: 'abcd',
-    minZoom: 5,
-    maxZoom: 18,  
+L.tileLayer('https://tiles.stadiamaps.com/tiles/alidade_smooth/{z}/{x}/{y}{r}.png', { minZoom: 5, maxZoom: 18,
     attribution: '&copy; <a href="https://stadiamaps.com/">Stadia Maps</a>, &copy; <a href="https://openmaptiles.org/">OpenMapTiles</a> &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors'
+}).addTo(map);
 
+new L.GeoJSON.AJAX("data/ukr_shape.geojson",{
+    style: {
+        fillColor: 'transparent',
+        weight: 1,
+        opacity: 1,
+        color: 'grey',  //Outline color
+        fillOpacity: 0.7
+    }
 }).addTo(map);
 
 map.attributionControl.setPosition('bottomleft');
 map.zoomControl.setPosition('bottomright');
 
+
 var loader = new PIXI.loaders.Loader();
-
 loader
-    .add('blue', 'img/right-arrow_green.png')
-    .add('red', 'img/right-arrow(1).png');
-
-
-// var geojson = new L.GeoJSON.ajax("data/ukraine.geojson");
-
-
-
-
-
-// Define a style
-var myStyle = {
-    fillColor: 'transparent',
-    weight: 1,
-    opacity: 1,
-    color: 'grey',  //Outline color
-    fillOpacity: 0.7
-};
-
-// Add the style to your layer
-var geojson = new L.GeoJSON.AJAX("data/ukr_shape.geojson",{
-    style:myStyle});
-
- geojson.addTo(map);
-
-
-
-
+    .add('blue', 'img/blue.png')
+    .add('red', 'img/red.png')
+    .add('green', 'img/green.png');
 
 var markerSprites = [];
-
+var markerSprites_slugi = [];
 var pixiContainer = new PIXI.Container();
+var greenMarkersContainer = new PIXI.Container();
 
 var zoomChangeTs = null;
+var tempLayer = new L.LayerGroup();
+var slugiLayer;
 
-var colorScale = d3.scaleLinear()
-    .domain([0, 50, 100])
-    .range(["#c6233c", "#ffd300", "#008000"]);
 
 document.addEventListener("DOMContentLoaded", function() {
 
     loader.load(function(loader, resources) {
 
-        var textures = [resources.blue.texture, resources.red.texture];
+        var textures = [resources.blue.texture, resources.red.texture, resources.green.texture];
 
         getJSON('data/fucking_end.json', function (markers) {
 
@@ -96,17 +74,14 @@ document.addEventListener("DOMContentLoaded", function() {
                 d.Longitude = +d.Longitude
             });
 
-            //function draw() {
-
             var pixiLayer = function () {
-
                 return L.pixiOverlay(function (utils, event) {
                     var zoom = utils.getMap().getZoom();
                     var container = utils.getContainer();
                     var renderer = utils.getRenderer();
                     var project = utils.latLngToLayerPoint;
                     var getScale = utils.getScale;
-                    var invScale = 0.5 / getScale();
+                    var invScale = 0.8 / getScale();
 
                     if (event.type === 'add') {
                         markers.forEach(function (marker) {
@@ -131,7 +106,11 @@ document.addEventListener("DOMContentLoaded", function() {
                             markerSprite.x = coords.x;
                             markerSprite.y = coords.y;
                             markerSprite.anchor.set(0.5, 0.5);
-                            markerSprite.scale.set(invScale);
+                            // if(invScale > 5){
+                                markerSprite.scale.set(2);
+                            // } else {
+                            //     markerSprite.scale.set(invScale);
+                            // }
                             // var tint = d3.color(colorScale(Math.random() * 100)).rgb();
                             // markerSprite.tint = 230 * (tint.r * 230 + tint.g) + tint.b;
                             container.addChild(markerSprite);
@@ -140,7 +119,6 @@ document.addEventListener("DOMContentLoaded", function() {
                             // Тут можна в спрайт упакувати ті дані, що потрібно
                             markerSprite.info = [marker["Polling_station"], marker['data06'], marker['data07'], marker['data12'], marker['data14'],  marker['data19'] ];
                             markerSprite.rotation -= radians
-
                         });
                     }
 
@@ -168,39 +146,6 @@ document.addEventListener("DOMContentLoaded", function() {
                     }
 
 
-                    //Функція яку можна запускати глобально, щоб повертати стрілочки. Поки вона їх повертає моментально. Ти даєш їй число і вона повертає на стільки.
-                    function smth(number) {
-
-                        var delta = number;
-
-                        // функція крутить rotate спрайт і змінює його текстуру (якщо він перейшов якщо підтримка одного блоку перевалила за середину)
-                        markerSprites.forEach(function (markerSprite) {
-                            markerSprite.rotation -= delta;
-
-                            //поки, що кожен раз коли ти запускаєш
-                            markerSprite.setTexture(textures[1])
-                        });
-
-                        if (zoomChangeTs !== null) {
-                            var duration = 17;
-                            zoomChangeTs += delta;
-                            var lambda = zoomChangeTs / duration;
-                            if (lambda > 1) {
-                                lambda = 1;
-                                zoomChangeTs = null;
-                            }
-                            lambda = easing(lambda);
-                            markerSprites.forEach(function (markerSprite) {
-                                markerSprite.scale.set(markerSprite.currentScale + lambda * (markerSprite.targetScale - markerSprite.currentScale));
-                            });
-                        }
-                        else {
-                            markerSprites.forEach(function (markerSprite) {
-                                markerSprite.scale.set(invScale);
-                            });
-                        }
-                    }
-
 
                     renderer.render(container);
                 }, pixiContainer, {
@@ -219,37 +164,133 @@ document.addEventListener("DOMContentLoaded", function() {
             });
 
             ticker.start();
-
             map.on('zoomanim', pixiLayer.redraw, pixiLayer);
-
         });
 
 
+    //////////////////////////////////////////////////////////////////////////
+
+
+
+        getJSON('data/slugi_2019.json', function (markers) {
+
+            markers.forEach(function(d){
+                d.Latitude = +d.Latitude;
+                d.Longitude = +d.Longitude
+            });
+
+            slugiLayer = function () {
+                return L.pixiOverlay(function (utils, event) {
+                    var zoom = utils.getMap().getZoom();
+                    var container = utils.getContainer();
+                    var renderer = utils.getRenderer();
+                    var project = utils.latLngToLayerPoint;
+                    var getScale = utils.getScale;
+                    var invScale = 0.8 / getScale();
+
+                    if (event.type === 'add') {
+                        markers.forEach(function (marker) {
+                            var proEuropePercentage = +marker['data19'];
+
+                            //Кут для проєвропейських
+                            var radians = Math.PI / 100 * 50;
+                            var markerSprite;
+
+
+                            markerSprite = new PIXI.Sprite(textures[2]);
+
+                            if (proEuropePercentage != undefined){
+                                markerSprite.alpha = 1;
+                                markerSprite.texture = textures[2];
+                                markerSprite.rotation = 0;
+                            } else {
+                                markerSprite.alpha = 0;
+                            }
+
+                            markerSprite.textureIndex = 2;
+
+
+                            var coords = project([marker.Latitude, marker.Longitude]);
+                            markerSprite.x = coords.x;
+                            markerSprite.y = coords.y;
+                            markerSprite.anchor.set(0.5, 0.5);
+                            if(invScale > 5){
+                                markerSprite.scale.set(5);
+                            } else {
+                                markerSprite.scale.set(invScale);
+                            }
+
+                            container.addChild(markerSprite);
+                            markerSprites_slugi.push(markerSprite);
+
+                            // Тут можна в спрайт упакувати ті дані, що потрібно
+                            markerSprite.info = [marker["Polling_station"], marker['data19'] ];
+                            markerSprite.rotation -= radians
+                        });
+                    }
+                    if (event.type === 'redraw') {
+                        var delta = event.delta;
+                        if (zoomChangeTs !== null) {
+                            var duration = 17;
+                            zoomChangeTs += delta;
+                            var lambda = zoomChangeTs / duration;
+                            if (lambda > 1) {
+                                lambda = 1;
+                                zoomChangeTs = null;
+                            }
+                            lambda = easing(lambda);
+                            markerSprites_slugi.forEach(function (markerSprite) {
+                                markerSprite.scale.set(markerSprite.currentScale + lambda * (markerSprite.targetScale - markerSprite.currentScale));
+                            });
+                        }
+                        else {
+                            markerSprites_slugi.forEach(function (markerSprite) {
+                                markerSprite.scale.set(invScale);
+                            });
+                        }
+                    }
+                renderer.render(container);
+                }, greenMarkersContainer, {
+                    // doubleBuffering: doubleBuffering,
+                    destroyInteractionManager: true
+                });
+            } ();
+
+
+            slugiLayer.addTo(tempLayer);
+
+            var ticker = new PIXI.ticker.Ticker();
+
+            ticker.add(function (delta) {
+                slugiLayer.redraw({type: 'redraw', delta: delta});
+            });
+
+            ticker.start();
+            map.on('zoomanim', slugiLayer.redraw, slugiLayer);
+        });
+
+
+
+
+
+    ////////////////////////////////////////////////////////////////////////
+
         function changeData(n) {
                 markerSprites.forEach(function(p, i){
-
-
                     var markerSprite = markerSprites[i];
-
-
-
                     var proEuropePercentage = markerSprite.info[n];
-                    if(proEuropePercentage != undefined){
+
+                    if (proEuropePercentage != undefined){
                         markerSprite.alpha = 1;
-                        //Кут для проєвропейських
                         var radians = Math.PI / 100 * proEuropePercentage;
 
                         if (radians < Math.PI / 2) {
-                            markerSprite.texture = textures[1];
+                             markerSprite.texture = textures[1];
                         }
                         else {
                             markerSprite.texture = textures[0];
                         }
 
-                        // var tint = d3.color(colorScale(Math.random() * 100)).rgb();
-                        // markerSprite.tint = 256 * (tint.r * 256 + tint.g) + tint.b;
-                        // var tint = d3.color(colorScale(Math.random() * 100)).rgb();
-                        // markerSprite.tint = 256 * (tint.r * 256 + tint.g) + tint.b;
                         markerSprite.rotation =  0;
                         markerSprite.rotation -= radians;
                     } else {
@@ -261,11 +302,11 @@ document.addEventListener("DOMContentLoaded", function() {
         }
 
 
-        document.getElementById("e_2006").addEventListener("click", function(){  changeData(1) });
-        document.getElementById("e_2007").addEventListener("click", function(){  changeData(2) });
-        document.getElementById("e_2012").addEventListener("click", function(){  changeData(3) });
-        document.getElementById("e_2014").addEventListener("click", function(){  changeData(4) });
-        document.getElementById("e_2019").addEventListener("click", function(){  changeData(5) });
+        document.getElementById("e_2006").addEventListener("click", function(){  changeData(1); greenMarkersContainer.alpha = 0; });
+        document.getElementById("e_2007").addEventListener("click", function(){  changeData(2); greenMarkersContainer.alpha = 0; });
+        document.getElementById("e_2012").addEventListener("click", function(){  changeData(3); greenMarkersContainer.alpha = 0; });
+        document.getElementById("e_2014").addEventListener("click", function(){  changeData(4); greenMarkersContainer.alpha = 0; });
+        document.getElementById("e_2019").addEventListener("click", function(){  changeData(5); tempLayer.addTo(map); greenMarkersContainer.alpha = 1; });
 
 
 
@@ -275,3 +316,34 @@ document.addEventListener("DOMContentLoaded", function() {
 });
 
 
+//
+// //Функція яку можна запускати глобально, щоб повертати стрілочки. Поки вона їх повертає моментально. Ти даєш їй число і вона повертає на стільки.
+// function smth(number) {
+//     var delta = number;
+//     // функція крутить rotate спрайт і змінює його текстуру (якщо він перейшов якщо підтримка одного блоку перевалила за середину)
+//     markerSprites.forEach(function (markerSprite) {
+//         markerSprite.rotation -= delta;
+//
+//         //поки, що кожен раз коли ти запускаєш
+//         markerSprite.setTexture(textures[1])
+//     });
+//
+//     if (zoomChangeTs !== null) {
+//         var duration = 17;
+//         zoomChangeTs += delta;
+//         var lambda = zoomChangeTs / duration;
+//         if (lambda > 1) {
+//             lambda = 1;
+//             zoomChangeTs = null;
+//         }
+//         lambda = easing(lambda);
+//         markerSprites.forEach(function (markerSprite) {
+//             markerSprite.scale.set(markerSprite.currentScale + lambda * (markerSprite.targetScale - markerSprite.currentScale));
+//         });
+//     }
+//     else {
+//         markerSprites.forEach(function (markerSprite) {
+//             markerSprite.scale.set(invScale);
+//         });
+//     }
+// }
