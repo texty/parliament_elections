dbe5fc96fc5979d63fe08e064caeaca55288733f
Yevheniia Drozdova <evgesha.drozdova@gmail.com>
2020-05-06 21:24:05 +0300

    додала зелений лаєр для 2019 року

--- js/testDataScript.js
+++ js/testDataScript.js
@@ -51,7 +51,9 @@ $(window).bind('mousewheel DOMMouseScroll', function (event) {
 
 
 // додаємо тайли
-L.tileLayer('https://tiles.stadiamaps.com/tiles/alidade_smooth/{z}/{x}/{y}{r}.png', { minZoom: 5, maxZoom: 18,
+L.tileLayer('https://tiles.stadiamaps.com/tiles/alidade_smooth/{z}/{x}/{y}{r}.png', {
+    minZoom: 5,
+    maxZoom: 18,
     attribution: '&copy; <a href="https://stadiamaps.com/">Stadia Maps</a>, &copy; <a href="https://openmaptiles.org/">OpenMapTiles</a> &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors'
 }).addTo(map);
 
