fcecceca3e88d71c777b54c4f6640fe661581058
Yevheniia Drozdova <evgesha.drozdova@gmail.com>
2020-06-09 22:04:48 +0300

    запушила векторні тайли

--- js_new/vector_tiles.js
+++ js_new/vector_tiles.js
@@ -27,23 +27,57 @@ var map = new mapboxgl.Map({
             tiles: ["https://texty.github.io/parliament_elections/source_06/{z}/{x}/{y}.pbf"]
         });
 
+        var url = 'img/headarrow.png';
+
+        map.loadImage(url, function(err, image) {
+            if (err) {
+                console.error('err image', err);
+                return;
+            }
+
+
+            map.addImage('arrow', image);
+
         var results_layer = map.addLayer({
-            "id": "election_districts",
+            "id": "election_data",
             'type': 'line',
             "source": "election_districts",
             "source-layer": "example_4326",
             'layout': {
+
                 'line-join': 'round',
                 'line-cap': 'round'
+
+
             },
             "paint": {
-                'line-color': '#ff69b4',
-                'line-width': 1
+                'line-color': ["get",  "color_2006"],
+                'line-width': 2
             }
         }, 'place_other');
 
 
 
+            map.addLayer({
+                'id': 'arrow-layer',
+                'type': 'symbol',
+                'source': 'election_districts',
+                "source-layer": "example_4326",
+                'layout': {
+                    'symbol-placement': 'point',
+                    'symbol-spacing': 1,
+                    'icon-allow-overlap': true,
+                    'icon-ignore-placement': true,
+                    'icon-image': 'arrow',
+                    'icon-size': 0.5,
+                    'visibility': 'visible',
+                    'icon-rotate': "lineBearing"
+
+                }
+            });
+
+
+        });
 
 
 
@@ -56,4 +90,8 @@ var map = new mapboxgl.Map({
 
 
 
-});
+
+
+
+
+    });
