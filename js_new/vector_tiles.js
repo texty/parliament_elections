41fca9e40567fc5270909675f69887c2c9db5e68
Yevheniia Drozdova <evgesha.drozdova@gmail.com>
2020-06-10 10:23:51 +0300

    тестові тайли з маркерами-стрілками

--- js_new/vector_tiles.js
+++ js_new/vector_tiles.js
@@ -4,20 +4,47 @@
 mapboxgl.accessToken = 'pk.eyJ1IjoiZHJpbWFjdXMxODIiLCJhIjoiWGQ5TFJuayJ9.6sQHpjf_UDLXtEsz8MnjXw';
 var map = new mapboxgl.Map({
     container: 'map',
-    style: 'style3.json',
+    // style: 'style3.json',
     minZoom: 5, //restrict map zoom
     maxZoom: 10,
-    zoom: 5,
-    center: [32.259271, 48.518688],
+    // zoom: 5,
+    // center: [32.259271, 48.518688],
     hash: false,
-    tap: false
+    tap: false,
+    attributionControl: false,
+    style: {
+        'version': 8,
+        'sources': {
+            'raster-tiles': {
+                'type': 'raster',
+                'tiles': [
+                    'https://api.mapbox.com/styles/v1/evgeshadrozdova/cjsqjh1to30c81ftn8jnuikgj/tiles/256/{z}/{x}/{y}?access_token=pk.eyJ1IjoiZXZnZXNoYWRyb3pkb3ZhIiwiYSI6ImNqMjZuaGpkYTAwMXAzMm5zdGVvZ2c0OHYifQ.s8MMs2wW15ZyUfDhTS_cdQ'
+                ],
+                'tileSize': 256,
+                'attribution':
+                    'Map tiles by <a target="_top" rel="noopener" href="http://stamen.com">Stamen Design</a>, under <a target="_top" rel="noopener" href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a>. Data by <a target="_top" rel="noopener" href="http://openstreetmap.org">OpenStreetMap</a>, under <a target="_top" rel="noopener" href="http://creativecommons.org/licenses/by-sa/3.0">CC BY SA</a>'
+            }
+        },
+        'layers': [
+            {
+                'id': 'simple-tiles',
+                'type': 'raster',
+                'source': 'raster-tiles',
+                'minzoom': 5,
+                'maxzoom': 10
+            }
+        ]
+    },
+    center: [32.259271, 48.518688],
+    zoom: 5 // starting zoom
 });
 
 
 
     map.scrollZoom.disable();
 
-    map.on('load', function () {
+
+map.on('load', function () {
 
         map.addSource('election_districts', {
             type: 'vector',
@@ -194,9 +221,11 @@ var map = new mapboxgl.Map({
                 'line-color': ["get",  "color_2006"],
                 'line-width': 1
             }
-        }, 'place_other');
+        });
 
 
+    var nav = new mapboxgl.NavigationControl();
+    map.addControl(nav, 'top-left');
 
 
 
